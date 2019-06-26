c8506d19d89dcc290bde01c02d1a99d9c89ec6f2
Sehee Lee <sehee_elicer@elicer.com>
2019-06-26 21:32:41 +0900

    add MainPage.js, IndexHeader.js Components

---
+++ src/Components/IndexHeader/IndexHeader.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./IndexHeader.scss";
+class IndexHeader extends Component {
+  render() {
+    return (
+      <div className="IndexHeader">
+        <div className="index_header_wrapper header" />
+      </div>
+    );
+  }
+}
+
+export default IndexHeader;
---
+++ src/Pages/MainPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import IndexHeader from "../Components/IndexHeader/IndexHeader";
+import "./MainPage.scss";
+
+class MainPage extends Component {
+  render() {
+    return (
+      <div className="MainPage">
+        <IndexHeader />
+        <div className="main_page_content_wrapper">
+          <div className="main_page_title">Data Compare App</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
